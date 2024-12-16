db486d840c3f495ebc827db21309ab0b53646d11
João Carlos Vieira Filho <me.contrate.aero@gmail.com>
2024-12-16 01:16:02 +0000

    mensage, dia 4

---
+++ .next/server/pages/index.js
@@ -0,0 +1,57 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/index";
+exports.ids = ["pages/index"];
+exports.modules = {
+
+/***/ "./pages/style.module.scss":
+/*!*********************************!*\
+  !*** ./pages/style.module.scss ***!
+  \*********************************/
+/***/ ((module) => {
+
+eval("// Exports\nmodule.exports = {\n\t\"home\": \"style_home__JS9GH\",\n\t\"frase\": \"style_frase__dk_bS\",\n\t\"blink\": \"style_blink__NpF5F\"\n};\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9zdHlsZS5tb2R1bGUuc2Nzcy5qcyIsIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90YWJuZXdzMjAyNS8uL3BhZ2VzL3N0eWxlLm1vZHVsZS5zY3NzPzM4OGEiXSwic291cmNlc0NvbnRlbnQiOlsiLy8gRXhwb3J0c1xubW9kdWxlLmV4cG9ydHMgPSB7XG5cdFwiaG9tZVwiOiBcInN0eWxlX2hvbWVfX0pTOUdIXCIsXG5cdFwiZnJhc2VcIjogXCJzdHlsZV9mcmFzZV9fZGtfYlNcIixcblx0XCJibGlua1wiOiBcInN0eWxlX2JsaW5rX19OcEY1RlwiXG59O1xuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./pages/style.module.scss\n");
+
+/***/ }),
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _style_module_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./style.module.scss */ \"./pages/style.module.scss\");\n/* harmony import */ var _style_module_scss__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_style_module_scss__WEBPACK_IMPORTED_MODULE_1__);\n\n\nfunction Home() {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"div\", {\n        className: (_style_module_scss__WEBPACK_IMPORTED_MODULE_1___default().home),\n        children: /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"h1\", {\n            className: (_style_module_scss__WEBPACK_IMPORTED_MODULE_1___default().frase),\n            children: \"A vida nos cruzou por um instante, mas marcou para sempre.\"\n        }, void 0, false, {\n            fileName: \"/workspaces/tabnews2025/pages/index.js\",\n            lineNumber: 6,\n            columnNumber: 9\n        }, this)\n    }, void 0, false, {\n        fileName: \"/workspaces/tabnews2025/pages/index.js\",\n        lineNumber: 5,\n        columnNumber: 7\n    }, this);\n}\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Home);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9pbmRleC5qcy5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7QUFBeUM7QUFFekMsU0FBU0MsT0FBTztJQUNaLHFCQUNFLDhEQUFDQztRQUFJQyxXQUFXSCxnRUFBVztrQkFDekIsNEVBQUNLO1lBQUdGLFdBQVdILGlFQUFZO3NCQUFFOzs7Ozs7Ozs7OztBQUduQztBQUVGLGlFQUFlQyxJQUFJQSxFQUFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdGFibmV3czIwMjUvLi9wYWdlcy9pbmRleC5qcz9iZWU3Il0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBzdHlsZXMgZnJvbSAnLi9zdHlsZS5tb2R1bGUuc2Nzcyc7XG5cbmZ1bmN0aW9uIEhvbWUoKSB7XG4gICAgcmV0dXJuIChcbiAgICAgIDxkaXYgY2xhc3NOYW1lPXtzdHlsZXMuaG9tZX0+XG4gICAgICAgIDxoMSBjbGFzc05hbWU9e3N0eWxlcy5mcmFzZX0+QSB2aWRhIG5vcyBjcnV6b3UgcG9yIHVtIGluc3RhbnRlLCBtYXMgbWFyY291IHBhcmEgc2VtcHJlLjwvaDE+XG4gICAgICA8L2Rpdj5cbiAgICApO1xuICB9XG5cbmV4cG9ydCBkZWZhdWx0IEhvbWU7Il0sIm5hbWVzIjpbInN0eWxlcyIsIkhvbWUiLCJkaXYiLCJjbGFzc05hbWUiLCJob21lIiwiaDEiLCJmcmFzZSJdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./pages/index.js\n");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/index.js"));
+module.exports = __webpack_exports__;
+
+})();
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import styles from './style.module.scss';
+
+function Home() {
+    return (
+      <div className={styles.home}>
+        <h1 className={styles.frase}>A vida nos cruzou por um instante, mas marcou para sempre.</h1>
+      </div>
+    );
+  }
+
+export default Home;
