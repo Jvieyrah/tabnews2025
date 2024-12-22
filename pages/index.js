18f28185a58c19d8babc0ced7e86f20341b825ee
João Carlos Vieira Filho <me.contrate.aero@gmail.com>
2024-12-22 03:00:00 +0000

    adiciona script `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
-import styles from './style.module.scss';
+import styles from "./style.module.scss";
 
 function Home() {
-    return (
-      <div className={styles.home}>
-        <h1 className={styles.frase}>A vida nos cruzou por um instante, mas marcou para sempre.</h1>
-      </div>
-    );
-  }
+  return (
+    <div className={styles.home}>
+      <h1 className={styles.frase}>
+        A vida nos cruzou por um instante, mas marcou para sempre.
+      </h1>
+    </div>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function test2() {
+  console.log("teste2");
+}
 
 export default Home;
