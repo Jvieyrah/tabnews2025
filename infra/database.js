0953c78eed4c8d46c464eef1e136b9948fb312fa
João Carlos Vieira Filho <me.contrate.aero@gmail.com>
2025-01-07 01:04:36 +0000

    makes db client resilient to errors

--- infra/database.js
+++ infra/database.js
@@ -8,6 +8,12 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
   });
+
+  comsole.log ("variaveis de ambiente: ",     host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD, )
   try {
     await client.connect();
     const result = await client.query(queryObject);
