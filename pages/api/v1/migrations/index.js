713efd3bad79f838fa6e6cc83b2b0e27ad3d8ae3
João Carlos Vieira Filho <me.contrate.aero@gmail.com>
2025-01-11 22:13:34 +0000

    add endpoint '/migrations' withm method post

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,13 +2,20 @@ import MigrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 
 export default async function migrations(request, response) {
-  const migrations = MigrationRunner({
+  if (!["GET", "POST"].includes(request.method)) {
+    return response.status(405).end(); // Method Not Allowed
+  }
+
+  const dryRun = request.method === "GET";
+
+  const migrations = await MigrationRunner({
     databaseUrl: process.env.DATABASE_URL,
-    dryRun: true,
+    dryRun,
     dir: join("infra", "migrations"),
     direction: "up",
     verbose: true,
     migrationsTable: "pgmigrations",
   });
-  response.status(200).json([migrations]);
+
+  return response.status(200).json(migrations);
 }
