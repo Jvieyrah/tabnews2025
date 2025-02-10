d60e48cdf41ee1a0a38f9f6fc89a02d752e327a0
João Carlos Vieira Filho <me.contrate.aero@gmail.com>
2025-02-10 01:12:37 +0000

    feat: implement endpoint status

---
+++ pages/status/index.js
@@ -0,0 +1,51 @@
+import useSwr from "swr";
+
+async function fetchApi(key) {
+  const response = await fetch(key);
+  const responseBody = await response.json();
+  return responseBody;
+}
+
+function Status() {
+  const response = useSwr("/api/v1/status", fetchApi, {
+    refreshInterval: 2000,
+    dedumpingInterval: 2000,
+  });
+  return (
+    <>
+      <h1>Status</h1>
+      <UpdatedAt />
+    </>
+  );
+}
+
+function UpdatedAt() {
+  const { data, isLoading } = useSwr("/api/v1/status", fetchApi, {
+    refreshInterval: 2000,
+    dedumpingInterval: 2000,
+  });
+
+  const loadText = "carregando ...";
+
+  let updatedAtText = loadText;
+  let dbVersionText = loadText;
+  let maxConnectionsText = loadText;
+  let oppenedConnectionsText = loadText;
+
+  if (!isLoading && data) {
+    let updatedAtText = new Date(data.updated_at).toLocaleString("pr-BR");
+    let dbVersionText = data.dependencies.database.version;
+    let maxConnectionsText = data.dependencies.database.max_connections;
+    let oppenedConnectionsText = data.dependencies.database.openned_connections;
+  }
+  return (
+    <div>
+      <p>Ùltima atualização {updatedAtText}</p>
+      <p>Versão do postgress {dbVersionText}</p>
+      <p>Maximo de conexôes {maxConnectionsText}</p>
+      <p>Conexões abertas {oppenedConnectionsText}</p>
+    </div>
+  );
+}
+
+export default Status;
